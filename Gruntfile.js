8d224b53ceb12980807f487e4fcced5f57b59a15
André Mendonça <andre.mendonca@atende.local>
2016-06-08 10:36:16 -0300

    Concat e Uglify do JS add

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,46 +1,20 @@
-module.exports = function (grunt) {
-    grunt.initConfig({
+module.exports = function(grunt) {
+  grunt.loadNpmTasks("grunt-contrib-concat");
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
-        cssmin: {
-            target: {
-                files: {
-                    'deploy/style.min.css': 'public/stylesheets/mystyle.css'
-                }
-            }
-        },
+  grunt.initConfig({
+          concat: {
+                  dist: {
+                          src: ["site/src/js/*.js"],
+                          dest: "site/res/js/app.js"
+                  }
+          },
+          uglify: {
+                  bundle: {
+                          files: {'site/res/js/app.min.js': 'site/res/js/app.js'}
+                  }
+          }
+  });
 
-        less: {
-            compile: {
-                files: {
-                    'deploy/compiled.css': 'public/stylesheets/layout.less'
-                }
-            }
-        },
-
-        jshint: {
-            jsFiles: ['public/javascripts/**/*.js']
-        },
-
-        concat: {
-            js: {
-                files: {'deploy/bundle.js': 'public/javascripts/**/*.js' }
-            }
-        },
-
-        uglify: {
-            bundle: {
-                files: {'deploy/bundle.min.js': 'deploy/bundle.js'}
-            }
-        }
-    });
-
-    // Next one would load plugins
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-
-    // Here is where we would define our task
-    grunt.registerTask('default', ['cssmin:target', 'less:compile', 'jshint:jsFiles', 'concat:js', 'uglify:bundle']);
+grunt.registerTask('default', ['concat', 'uglify:bundle']);
 };
